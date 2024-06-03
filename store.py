cf2b1d54a8b1bdbf187cc3bf2364d2a8262f2e92
Mami K <49592804+carveler@users.noreply.github.com>
2024-06-03 17:47:59 +0200

    fix: style error

--- store.py
+++ store.py
@@ -1,11 +1,11 @@
 class Store:
     """
     The Store class represents a store that sells products.
-    It manages the inventory of products and provides 
+    It manages the inventory of products and provides
     methods to add, remove, and order products.
 
     Attributes:
-    - list_products (list): A list of Product objects 
+    - list_products (list): A list of Product objects
     representing the products available in the store.
     """
 
@@ -14,7 +14,7 @@ class Store:
         Initiator (constructor) method.
         Initializes the Store object with a list of products.
 
-        :param list_products: list - A list of Product 
+        :param list_products: list - A list of Product
         objects representing the products available in the store.
         """
         self.list_products = list_products
@@ -25,7 +25,7 @@ class Store:
 
         :param product: Product - The Product object to add to the store.
         """
-        self.products.append(product)
+        self.list_products.append(product)
 
     def remove_product(self, product):
         """
@@ -34,7 +34,7 @@ class Store:
         :param product: Product - The Product object to remove from the store.
         """
         new_products = []
-        for item in self.products:
+        for item in self.list_products:
             if not product.name == item.name:
                 new_products.append(item)
         self.list_products = new_products
@@ -46,7 +46,7 @@ class Store:
         :return: int - The total quantity of items in the store.
         """
         total_quantity = 0
-        for item in self.products:
+        for item in self.list_products:
             total_quantity += item.quantity
         return total_quantity
 
@@ -54,18 +54,18 @@ class Store:
         """
         Returns a list of all active products in the store.
 
-        :return: list - A list of Product objects representing 
+        :return: list - A list of Product objects representing
         all active products in the store.
         """
         active_products = []
-        for item in self.products:
+        for item in self.list_products:
             if item.active is True:
                 active_products.append(item)
         return active_products
 
     def order(self, shopping_list):
         """
-        Processes an order based on the provided shopping list 
+        Processes an order based on the provided shopping list
         and returns the total price of the order.
 
         :param shopping_list: list - A list of tuples where
