c5096e04cfdd45c1ade62b2bca73a942e68044cd
Mami K <49592804+carveler@users.noreply.github.com>
2024-06-05 18:32:27 +0200

    feat: create new product type class

--- products.py
+++ products.py
@@ -1,20 +1,22 @@
 class Product:
-    '''
-    The Product class represents a specific type of product available in the store.
+    """
+    The Product class represents a specific type of product available
+    in the store.
     It encapsulates information about the product,
     including its name, price, quantity, and status (active or not).
-    '''
+    """
 
-    def __init__(self, name, price, quantity):
-        '''
+    def __init__(self, name, price, quantity=0):
+        """
         Initiator (constructor) method.
         Creates the instance variables (active is set to True).
-        If something is invalid (empty name / negative price or quantity), raises an exception.
+        If something is invalid (empty name / negative price or quantity),
+        raises an exception.
 
         :param name: str - The name of the product.
         :param price: float or int - The price of the product.
         :param quantity: int - The quantity of the product.
-        '''
+        """
         if not name:
             raise ValueError("Product name cannot be empty.")
         if not isinstance(name, str):
@@ -30,71 +32,144 @@ class Product:
 
         self.name = name
         self.price = float(price)
-        self.quantity = quantity
+        self._quantity = quantity
         self.active = True
 
+    @property
+    def quantity(self):
+        return self._quantity
+
+    @quantity.setter
+    def quantity(self, value):
+        if value < 0:
+            raise ValueError("Product quantity cannot be negative.")
+        if not isinstance(value, int):
+            raise TypeError("Quantity has to be an integer.")
+        self._quantity = value
+        if self._quantity == 0 and not isinstance(self, NonStockedProduct):
+            self.deactivate()
+
     def get_quantity(self):
-        '''
+        """
         get_quantity(self) -> float
         Getter function for quantity.
         Returns the quantity (float).
         :return: float - The quantity of the product.
-        '''
+        """
         return float(self.quantity)
 
-    def set_quantity(self, quantity):
-        '''
-        Setter function for quantity. If quantity reaches 0,
-        deactivates the product.
-        :param quantity: int - The new quantity of the product.
-        '''
-        if quantity == 0:
-            self.active = False
-        self.quantity = quantity
-
     def is_active(self):
-        '''
+        """
         Getter function for active status.
         Returns True if the product is active, otherwise False.
 
         :return: bool - The active status of the product.
-        '''
+        """
         return self.active
 
     def activate(self):
-        '''
+        """
         Activates the product by setting active to True.
-        '''
+        """
         self.active = True
 
     def deactivate(self):
-        '''
+        """
         Deactivates the product by setting active to False.
-        '''
+        """
         self.active = False
 
     def show(self):
-        '''
+        """
         Prints the product details (name, price, quantity).
-        '''
+        """
         print(f"{self.name}, {self.price}, {self.quantity}")
 
-    def buy(self, quantity):
-        '''
+    def buy(self, value):
+        """
         Buys a given quantity of the product.
         Returns the total price (float) of the purchase.
         Updates the quantity of the product.
         Raises an exception if the quantity is not an integer,
         exceeds available stock, or is less than or equal to zero.
 
-        :param quantity: int - The quantity to buy.
+        :param value: int - The quantity to buy.
         :return: float - The total price of the purchase.
-        '''
-        if not isinstance(quantity, int):
+        """
+        if not isinstance(value, int):
             raise TypeError("Quantity has to be an integer.")
-        if quantity > self.quantity:
-            raise ValueError("Not enough stock available")
-        if quantity <= 0:
+        if value > self.quantity:
+            raise ValueError("Not enough stock available.")
+        if value <= 0:
             raise ValueError("Quantity has to be greater than zero.")
-        self.set_quantity(self.quantity - quantity)
-        return self.price * quantity
+        self.quantity -= value
+        return self.price * value
+
+
+class NonStockedProduct(Product):
+    """
+    the quantity should be set to zero always
+    """
+
+    def __init__(self, name, price):
+        super().__init__(name, price, quantity=0)
+
+    @property
+    def quantity(self):
+        return 0
+
+    @quantity.setter
+    def quantity(self, value):
+        raise AttributeError("NonStockedProduct quantity cannot be modified.")
+
+    def show(self):
+        """
+        Prints the product details (name, price, quantity).
+        """
+
+        print(
+            f"{self.name} (Non-Stocked), Price: {self.price}, "
+            f"Quantity: {self.quantity}"
+        )
+
+
+class LimitedProduct(Product):
+    """
+    shipping fee can only be added once.
+    """
+
+    def __init__(self, name, price, quantity, maximum):
+        super().__init__(name, price, quantity)
+        self.maximum = maximum
+
+    def show(self):
+        """
+        Prints the product details (name, price, quantity).
+        """
+        print(
+            f"{self.name}, {self.price}, {self.quantity},"
+            f" Limited item only possible to add {self.maximum} item"
+        )
+
+    def buy(self, value):
+        """
+        Buys a given quantity of the product.
+        Returns the total price (float) of the purchase.
+        Updates the quantity of the product.
+        Raises an exception if the quantity is not an integer,
+        exceeds available stock, or is less than or equal to zero.
+
+        :param value: int - The quantity to buy.
+        :return: float - The total price of the purchase.
+        """
+        if not isinstance(value, int):
+            raise TypeError("Quantity has to be an integer.")
+        if value > self.quantity:
+            raise ValueError("Not enough stock available.")
+        if value <= 0:
+            raise ValueError("Quantity has to be greater than zero.")
+        if value > self.maximum:
+            raise ValueError(f"{self.name} is limited to "
+                             f"add max {self.maximum}.")
+        self.quantity -= value
+        return self.price * value
--- test_product.py
+++ test_product.py
@@ -10,14 +10,21 @@ def test_normal_product():
 
 
 def test_inactive():
-    mac = products.Product("MacBook Air M2", price=10, quantity=100)
+    mac = products.Product("MacBook Air M2", price=1000, quantity=100)
     mac.buy(100)
+    assert mac.quantity == 0
     assert mac.active == False
 
 
 def test_quantity():
-    mac = products.Product("MacBook Air M2", price=10, quantity=100)
+    mac = products.Product("MacBook Air M2", price=1000, quantity=100)
     mac.buy(80)
+    assert mac.quantity == 20
+
+
+def test_buy():
+    mac = products.Product("MacBook Air M2", price=1000, quantity=100)
+    assert mac.buy(80) == 1000 * 80
     assert mac.quantity == 20
 
 
@@ -55,6 +62,98 @@ def test_quantity_int():
 def test_quantity_negative():
     with pytest.raises(ValueError, match="Product quantity cannot be negative."):
         products.Product("MacBook Air M2", price=10, quantity=-100)
+
+
+def test_buy_zero():
+    with pytest.raises(ValueError, match="Quantity has to be greater than zero."):
+        mac = products.Product("MacBook Air M2", price=10, quantity=100)
+        mac.buy(0)
+
+
+def test_buy_negative():
+    with pytest.raises(ValueError, match="Quantity has to be greater than zero."):
+        mac = products.Product("MacBook Air M2", price=10, quantity=100)
+        mac.buy(-1)
+
+
+def test_buy_exceed():
+    with pytest.raises(ValueError, match="Not enough stock available."):
+        mac = products.Product("MacBook Air M2", price=10, quantity=100)
+        mac.buy(200)
+
+
+def test_buy_string():
+    with pytest.raises(TypeError, match="Quantity has to be an integer."):
+        mac = products.Product("MacBook Air M2", price=10, quantity=100)
+        mac.buy("200")
+
+
+def test_non_stock_product():
+    product = products.NonStockedProduct("Windows License", price=125)
+    assert product.name == "Windows License"
+    assert product.price == 125
+    assert product.quantity == 0
+
+
+def test_always_zero():
+
+    with pytest.raises(
+        AttributeError, match="NonStockedProduct quantity cannot be modified."
+    ):
+        product = products.NonStockedProduct("Windows License", price=125)
+        product.quantity = 10
+
+
+def test_limited_product():
+    product = products.LimitedProduct("Shipping", price=10, quantity=250, maximum=1)
+    assert product.name == "Shipping"
+    assert product.price == 10
+    assert product.quantity == 250
+    assert product.maximum == 1
+
+
+def test_limit_exceed():
+    with pytest.raises(ValueError, match="Shipping is limted to add max 1."):
+        shipping = products.LimitedProduct(
+            "Shipping", price=10, quantity=250, maximum=1
+        )
+        shipping.buy(2)
+
+
+def test_limit_quantity_not_enough():
+    with pytest.raises(ValueError, match="Not enough stock available."):
+        shipping = products.LimitedProduct("Shipping", price=10, quantity=0, maximum=1)
+        shipping.buy(1)
+
+
+def test_limit_buy_zero():
+    with pytest.raises(ValueError, match="Quantity has to be greater than zero."):
+        shipping = products.LimitedProduct(
+            "Shipping", price=10, quantity=250, maximum=1
+        )
+        shipping.buy(0)
+
+
+def test_limit_buy_negative():
+    with pytest.raises(ValueError, match="Quantity has to be greater than zero."):
+        shipping = products.LimitedProduct(
+            "Shipping", price=10, quantity=250, maximum=1
+        )
+        shipping.buy(-1)
+
+
+def test_limit_buy_exceed():
+    with pytest.raises(ValueError, match="Not enough stock available."):
+        shipping = products.LimitedProduct(
+            "Shipping", price=10, quantity=150, maximum=1
+        )
+        shipping.buy(200)
+
+
+def test_limit_buy():
+    shipping = products.LimitedProduct("Shipping", price=10, quantity=150, maximum=1)
+    assert shipping.buy(1) == 10.0
+    assert shipping.quantity == 149
 
 
 pytest.main()
